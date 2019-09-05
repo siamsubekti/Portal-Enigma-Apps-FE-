39507b551dd5438b6712395f3e623d8fd36a4a5b
adisthya <adisthya.rahmadyan@gmail.com>
2019-09-05 11:40:02 +0700

    Development of Candidate Module.

---
+++ src/app/candidates/profile/components/candidate.profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-candidate-profile',
+  templateUrl: '../views/candidate-profile.component.html',
+  styleUrls: ['../views/candidate-profile.scss'],
+})
+export class CandidateProfileComponent {
+  constructor() {}
+}
---
+++ src/app/candidates/profile/services/candidate-profile.service.ts
@@ -0,0 +1,41 @@
+import { Observable, Observer } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpService } from '../../../shared/http/http.service';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Account, AccountProfile } from '../../../master/models';
+import { CandidateProfileResponse } from '../models/candidate-profile.model';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable()
+export class CandidateProfileService {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly http: HttpService,
+  ) {}
+
+  get profile(): Account {
+    return this.authService.user;
+  }
+
+  update(payload: AccountProfile): Observable<Account> {
+    return new Observable((observer: Observer<Account>) => {
+      this.http.put('CAND_PROFILE_UPDATE', payload)
+        .subscribe((response: CandidateProfileResponse) => {
+          const { status, data } = response;
+
+          if (status && status.code !== '200') {
+            const error: Error = new Error(status.description);
+            error.name = status.code;
+
+            observer.error(new HttpErrorResponse({ error, status: +status.code, statusText: status.description }));
+          } else {
+            this.authService.user = data;
+            observer.next(data);
+          }
+        }, (error) => {
+          console.error('CandidateService update error:', error);
+          observer.error(error);
+        });
+    });
+  }
+}
